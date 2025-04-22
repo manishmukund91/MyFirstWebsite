ef34c04cd951081fc43d96898c1019dc92910bd5
manishmukund91 <manishmukund91@gmail.com>
2025-04-21 21:50:25 -0400

    New changes

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-﻿const heading = document.querySelector('h1');
+﻿const heading = document.querySelector('h1'); // Get the heading element
 
 let position = 0;
-const speed = 2;
+const speed = 2; // How fast the heading moves
 
 function animateHeading() {
     position += speed;
-    heading.style.marginLeft = position + 'px';
+    heading.style.marginLeft = position + 'px'; // Move the heading to the right
 
-    if (position > window.innerWidth) {
-        position = -heading.offsetWidth;
+    // Get the width of the heading
+    const headingWidth = heading.offsetWidth;
+
+    // Get the width of the browser window
+    const windowWidth = window.innerWidth;
+
+    // Check if the heading has moved off the right side of the screen
+    if (position > windowWidth) {
+        // Reset the position to start from the left, just off-screen
+        position = -headingWidth;
     }
 
-    requestAnimationFrame(animateHeading);
+    requestAnimationFrame(animateHeading); // Call this function again for the next frame
 }
 
-animateHeading();
+animateHeading(); // Start the animation
